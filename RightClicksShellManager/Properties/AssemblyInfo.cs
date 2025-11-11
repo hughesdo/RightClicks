6aa1b3f65e3c5afbbd12b7a0ddd41d977efdcee3
Donald Hughes <hughes.donald464@gmail.com>
2025-11-11 14:59:18 -0500

    Phase 1 Task 1: Create Visual Studio solution and projects
    - Created RightClicks.sln solution file
    - Created RightClicks project (.NET 8 WPF)
    - Created RightClicksShellManager project (.NET Framework 4.8)
    - Added NuGet packages to RightClicks:
      * FFMpegCore 5.4.0
      * NAudio 2.2.1
      * Serilog 4.3.0
      * Serilog.Sinks.File 7.0.0
      * Newtonsoft.Json 13.0.4
    - Added NuGet packages to RightClicksShellManager:
      * SharpShell 2.7.2
      * Newtonsoft.Json 13.0.4
    - Created folder structure:
      * Features/ (Video, Audio, Image, Text)
      * Services/
      * Models/
      * Utilities/
      * Resources/
    - Verified solution builds successfully in Debug and Release

---
+++ RightClicksShellManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RightClicksShellManager")]
+[assembly: AssemblyDescription("Shell hook manager for RightClicks context menu extension")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RightClicks")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b8f3e8f2-9c4d-4a5b-8e3f-1d2c3b4a5e6f")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
